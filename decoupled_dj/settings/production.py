cb7dbb8671fb5980af6ac131043e26de1ceb3c3e
Scarnouse <manquesada@gmail.com>
2021-09-06 06:57:08 +0200

    Disable the browsable API

--- decoupled_dj/settings/production.py
+++ decoupled_dj/settings/production.py
@@ -1,12 +1,16 @@
 from decoupled_dj.settings.development import CORS_ALLOW_ALL_ORIGINS
 from .base import * # noqa
 
-SECURE_SSL_REDIRECT = True
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
-STATIC_ROOT = env("STATIC_ROOT")
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
 CORS_ALLOW_ALL_ORIGINS = env.list(
   "CORS_ALLOWED_ORIGINS",
   default=[]
 )
+CSRF_COOKIE_SECURE = True
+REST_FRAMEWORK = {
+  **REST_FRAMEWORK,
+  "DEFAULT_RENDERER_CLASSES": ["rest_framework.renderers.JSONrenderer"]
+}
+SESSION_COOKIE_SECURE = True
+STATIC_ROOT = env("STATIC_ROOT")
