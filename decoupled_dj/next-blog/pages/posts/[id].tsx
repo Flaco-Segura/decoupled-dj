f8883a122e0f5636877fca860aee20e90f84aee6
Scarnouse <manquesada@gmail.com>
2021-09-14 07:53:09 +0200

    Data Fetching

--- decoupled_dj/next-blog/pages/posts/[id].tsx
+++ decoupled_dj/next-blog/pages/posts/[id].tsx
@@ -1,3 +1,26 @@
+import { GetServerSideProps } from "next";
+
+const BASE_URL = "http://localhost:8000/blog/api";
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const id = params?.id;
+
+  const res = await fetch(`${BASE_URL}/posts/${id}`);
+
+  console.log(res);
+
+  if (!res.ok) {
+    return {
+      notFound: true
+    };
+  }
+
+  const json = await res.json();
+  const { title, body, created_at, status } = json;
+
+  return { props: { title, body, created_at, status } };
+}
+
 enum BlogPostStatus {
   Published = "PUBLISHED",
   Unpublished = "UNPUBLISHED",
