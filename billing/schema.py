8afdaae826142b0decb544399d39a38aaa4103b4
Scarnouse <manquesada@gmail.com>
2021-09-27 07:32:05 +0200

    Working with resolvers

--- billing/schema.py
+++ billing/schema.py
@@ -1,5 +1,6 @@
 from ariadne import load_schema_from_path, gql, make_executable_schema, ObjectType
 from pathlib import Path
+from users.models import User
 
 BASE_DIR = Path(__file__).resolve().parent
 
@@ -8,8 +9,8 @@ type_defs = gql(schema_file)
 
 query = ObjectType("Query")
 
-"""
-TODO: Write resolvers
-"""
+@query.field("getClients")
+def resolve_clients(obj, info):
+  return User.objects.all()
 
 schema = make_executable_schema(type_defs, query)
