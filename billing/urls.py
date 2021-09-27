6cabebd36e77576df924a265531538f2b62f3c26
Scarnouse <manquesada@gmail.com>
2021-09-27 07:22:42 +0200

    Wiring up the GraphQL endpoint

--- billing/urls.py
+++ billing/urls.py
@@ -1,6 +1,10 @@
+from os import name
 from django.urls import path
+import graphql
 from .views import Index
 from .api.views import ClientList, InvoiceCreate
+from ariadne.contrib.django.views import GraphQLView
+from .schema import schema
 
 app_name = "billing"
 
@@ -15,5 +19,6 @@ urlpatterns = [
     "api/invoices/",
     InvoiceCreate.as_view(),
     name="invoice-create"
-  )
+  ),
+  path("graphql/", GraphQLView.as_view(schema=schema), name=graphql),
 ]
