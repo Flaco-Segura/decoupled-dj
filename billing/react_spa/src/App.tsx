20b3541346f24d9a2f1b7f16da46817ef40904c8
Scarnouse <manquesada@gmail.com>
2021-10-05 07:43:31 +0200

    app.tsx modified

--- billing/react_spa/src/App.tsx
+++ billing/react_spa/src/App.tsx
@@ -1,10 +1,56 @@
+import React, { useEffect, useState } from "react";
 import {
   ApolloClient,
   InMemoryCache,
-  gql
+  gql,
 } from "@apollo/client";
+import Form from "./Form";
+import Select from "./Select";
 
 const client = new ApolloClient({
   uri: "http://127.0.0.1:8000/billing/graphql",
   cache: new InMemoryCache(),
 });
+
+const App = () => {
+  const [options, setOptions] = useState([
+    {id: "", email: ""},
+  ]);
+
+  const handleSubmit = (
+    event: React.FormEvent<HTMLFormElement>
+  ) => {
+    event.preventDefault();
+    // client.mutate()
+  };
+
+  const GET_CLIENTS = gql`
+    query getClients {
+      getClients {
+        id
+        email
+      }
+    }
+  `;
+
+  useEffect(() =>{
+    client
+      .query({
+        query: GET_CLIENTS,
+      })
+      .then(queryResult => {
+        setOptions(queryResult.data.getClients);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <Form handleSubmit={handleSubmit}>
+      <Select id="user" name="user" options={options} />
+    </Form>
+  );
+};
+
+export default App;
